0e21ae6016586cfe47ec60849520360a996307d5
Alfio <tomarchioalfio5@gmail.com>
2025-11-16 14:42:39 +0000

    add JSDoc comments

--- src/option.ts
+++ src/option.ts
@@ -1,46 +1,166 @@
-export type Option<T> = Some<T> | None;
+/**
+ * Type representing an optional value.
+ * 
+ * ### Example
+ * 
+ * ```ts
+ * function divide(numerator: number, denominator: number): Option<number> {
+ *     if(y === 0) {
+ *         return None;
+ *     } else {
+ *         return Some(numerator / denominator); 
+ *     }
+ * }
+ * 
+ * let result = divide(2.0, 3.0);
+ * result.match({
+ *     Some(n) { console.log(`Result: ${x}`); },
+ *     None()  { console.log("Cannot divide by 0")}
+ * })
+ * ```
+ */
+export type Option<T> = Some<T> | None<T>;
 
+/**
+ * Wrap a function that can throw, returning `Some(fn())` if the function returns, `None` if it throws.
+ * @param fn function that potentially throws
+ */
 export function as_option<T>(fn: () => T): Option<T> {
     try {
         return Some(fn());
     } catch (x) {
-        return None;
+        return None as None<T>;
     }
 }
 
 export type Some<T> = {
+    /**
+     * Return the contained `Some` value. Will throw a hard error with the specified message if called on `None`.
+     * @param message custom message to throw if `None`
+     */
+    expect(message: string): T;
+    /**
+     * returns `true` if the option is `Some`, `false` if `None`.
+     */
     is_some(): this is Some<T>;
-    is_none(): this is None;
-    map<U>(fn: (arg: T) => U): Some<U>
-    unwrap(): T,
-    unwrap_or(): T,
-    unwrap_or_else(): T
+    /**
+     * returns `false` if the option is `Some`, `true` if `None`.
+     */
+    is_none(): this is None<T>;
+    /**
+     * Maps an `Option<T>` into an `Option<U>`.
+     * Will return `Some(fn(value))` if the option is `Some`, or propagate `None`.
+     * @param fn function to transform the inner `value`
+     */
+    map<U>(fn: (arg: T) => U): Some<U>;
+    /**
+     * Pattern-match on the option, running the `Some` or `None` function in the respective cases.
+     * @param matcher object containing a `Some(value)` and `None(error)` function to run
+     */
+    match<R>(matcher: { Some(value: T): R, None(): R }): R;
+    /**
+     * Pattern-match on the option, running the former or latter function
+     * in the `Some` or `None` cases respectively. 
+     * @param on_some function to run if the option is `Some`
+     * @param on_none function to run if the option is `None`
+     */
+    match<R>(on_some: (value: T) => R, on_none: () => R): R;
+    /**
+     * Return the contained `Some` value. Will throw a hard error if called on `None`. 
+     */
+    unwrap(): T;
+    /**
+     * Return the contained `Some` value, or `default_value` if called on `None`.
+     * @param default_value value to use if the option is `None`
+     */
+    unwrap_or(default_value: T): T;
+    /**
+     * Return the contained `Some` value, or the result of `otherwise` if called on `None`.
+     * @param otherwise function to run if the option is `None`
+     */
+    unwrap_or_else(otherwise: () => T): T;
 }
-export type None = {
-    is_some<T>(): this is Some<T>;
-    is_none(): this is None;
-    map(): None
-    unwrap(_: never): never,
-    unwrap_or<T>(value: T): T,
-    unwrap_or_else<T>(otherwise: () => T): T
+export type None<T = unknown> = {
+    /**
+     * Return the contained `Some` value. Will throw a hard error with the specified message if called on `None`.
+     * @param message custom message to throw if `None`.
+     */
+    expect(message: string): never;
+    /**
+     * returns `true` if the option is `Some`, `false` if `None`.
+     */
+    is_some(): this is Some<T>;
+    /**
+     * returns `false` if the option is `Some`, `true` if `None`.
+     */
+    is_none(): this is None<T>;
+    /**
+     * Maps an `Option<T>` into an `Option<U>`.
+     * Will return `Some(fn(value))` if the option is `Some`, or propagate `None`.
+     * @param fn function to transform the inner `value`
+     */
+    map<U>(fn: (arg: T) => U): None<U>;
+    /**
+     * Pattern-match on the option, running the `Some` or `None` function in the respective cases.
+     * 
+     * @param matcher object containing a `Some(value)` and `None` function to run
+     */
+    match<R>(matcher: { Some(value: T): R, None(): R }): R;
+    /**
+     * Pattern-match on the option, running the former or latter function
+     * in the `Some(value)` and `None` cases respectively.
+     * 
+     * @param on_some function to run if the option is `Some`
+     * @param on_none function to run if the option is `None`
+     */
+    match<R>(on_some: (value: T) => R, on_none: () => R): R;
+    /**
+     * Return the contained `Some` value. Will throw a hard error if called on `None`. 
+     */
+    unwrap(): never;
+    /**
+     * Return the contained `Some` value, or `default_value` if the option is `None`.
+     * 
+     * @param default_value value to use if the option is `None`
+     */
+    unwrap_or(default_value: T): T;
+    /**
+     * Return the contained `Some` value, or the result of `otherwise` if the function is `None`.
+     * 
+     * @param otherwise function to run if the option is `None`
+     */
+    unwrap_or_else(otherwise: () => T): T;
 };
 
 export const Some = <T>(value: T) => ({
-    // TODO: expect
+    expect(_) { return value },
     is_some() { return true; },
     is_none() { return false; },
     map(fn) { return Some(fn(value)) },
+    match<R>(matcher_or_some) {
+        if ("Some" in matcher_or_some) {
+            return (matcher_or_some as { Some: (value: T) => R }).Some(value);
+        }
+        return matcher_or_some(value);
+    },
     unwrap() { return value; },
-    unwrap_or() { return value; },
-    unwrap_or_else() { return value; },
+    unwrap_or(_) { return value; },
+    unwrap_or_else(_) { return value; },
     toString() { return `Some(${value})`; }
 }) as Some<T>;
 export const None = Object.freeze({
+    expect(message) { throw Error(message); },
     is_some() { return false; },
     is_none() { return true; },
     map() { return None; },
+    match<R>(matcher_or_some, none: () => R) {
+        if ("None" in matcher_or_some) {
+            return (matcher_or_some as { None: () => R; }).None();
+        }
+        return none();
+    },
     unwrap(_) { throw Error("Cannot call unwrap on None"); },
-    unwrap_or(value) { return value; },
-    unwrap_or_else(otherwise) { return otherwise() },
+    unwrap_or(default_value) { return default_value; },
+    unwrap_or_else(otherwise) { return otherwise(); },
     toString() { return "None"; }
-}) as None;
+}) as unknown as None;
--- src/result.ts
+++ src/result.ts
@@ -1,5 +1,25 @@
+/**
+ * Type used to represent either a succesful value or an expected, recoverable error.
+ * 
+ * ### Example
+ * function parse_int(n: string): Result<number, string> {
+ *     let maybe = parseInt(n);
+ *     if(isNaN(maybe)) return Err("Could not parse input");
+ *     return Ok(maybe);
+ * }
+ * 
+ * let result = parse_int(3);
+ * result.match({
+ *     Ok(value)  { console.log("Parsed number: ", value) },
+ *     Err(error) { console.error("Error: ", error)}
+ * });
+ */
 export type Result<T, E> = Ok<T, E> | Err<T, E>;
 
+/**
+ * Wrap a function that can throw, returning `Ok(fn())` if the function returns, `Err(error)` if it throws `error`.
+ * @param fn function that potentially throws
+ */
 export function as_result<T>(fn: () => T): Result<T, unknown> {
     try {
         return Ok(fn());
@@ -9,39 +29,126 @@ export function as_result<T>(fn: () => T): Result<T, unknown> {
 }
 
 export type Ok<T, E> = {
+    /**
+     * Return the contained `Ok` value. Will throw a hard error with the specified message if called on `Err`.
+     * @param message custom message to throw if `None`
+     */
+    expect(message: string): T,
+    /**
+     * returns `true` if the option is `Ok`, `false` if `Err`.
+     */
     is_ok(): this is Ok<T, E>;
-    is_err(): this is Err<T, unknown>;
-    map<U>(fn: (arg: T) => U): Ok<U, E>;
-    map_err(): Ok<T, E>;
-    match<R>(matcher: { Ok(v: T): R, Err(e: E): R }): R;
-    match<R>(on_ok: (v: T) => R, on_err: (e: E) => R): R;
-    unwrap(): T;
-    unwrap_or(): T;
-    unwrap_or_else(): T;
-}
-export type Err<T, E> = {
-    is_ok(): this is Ok<unknown, E>;
+    /**
+     * returns `false` if the option is `Ok`, `true` if `Err`.
+     */
     is_err(): this is Err<T, E>;
-    map(): Err<T, E>;
-    map_err<F>(fn: (arg: E) => F): Err<T, F>;
-    match<R>(matcher: { Ok(v: T): R, Err(e: E): R }): R;
-    match<R>(on_ok: (v: T) => R, on_err: (e: E) => R): R;
-    unwrap(_: never): never;
+    /**
+     * Maps a `Result<T, E>` into a `Result<U, E>`.
+     * Will return `Ok(fn(value))` if the result is `Ok`, or propagate `Err`.
+     * @param fn function to transform the inner value
+     */
+    map<U>(fn: (arg: T) => U): Ok<U, E>;
+    /**
+     * Maps a `Result<T, E>` into a `Result<T, F>`.
+     * Will propagate `Ok`, or return `Err(fn(error))` if the result is `Err`.
+     * @param fn function to transform the inner value
+     */
+    map_err(): Ok<T, E>;
+    /**
+     * Pattern-match on the result, running the `Ok` or `Err` function in the respective cases.
+     * @param matcher object containing an `Ok(value)` and `Err(error)` function to run
+     */
+    match<R>(matcher: { Ok(value: T): R, Err(error: E): R }): R;
+    /**
+     * Pattern-match on the result, running the former or latter function
+     * in the `Ok` and `Err` cases respectively.
+     * 
+     * @param on_ok function to run if the result is `Ok(value)`
+     * @param on_err function to run if the option is `Err(error)`
+     */
+    match<R>(on_ok: (value: T) => R, on_err: (error: E) => R): R;
+    /**
+     * Return the contained `Ok` value. Will throw a hard error if called on `Err`. 
+     */
+    unwrap(): T;
+    /**
+     * Return the contained `Ok` value, or `default_value` if called on `Err`. 
+     * @param default_value value to use if the result is `Err`
+     */
     unwrap_or<T>(default_value: T): T;
+    /**
+     * Return the contained `Ok` value, or the result of `otherwise` if called on `Err`.
+     * @param otherwise function to use if the result is `Err`
+     */
+    unwrap_or_else<T>(otherwise: (err: E) => T): T;
+}
+
+export type Err<T, E> = {
+    /**
+     * Return the contained `Ok` value. Will throw a hard error with the specified message if called on `Err`.
+     * @param message custom message to throw if `None`
+     */
+    expect(message: string): never,
+    /**
+     * returns `true` if the option is `Ok`, `false` if `Err`.
+     */
+    is_ok(): this is Ok<T, E>;
+    /**
+     * returns `false` if the option is `Ok`, `true` if `Err`.
+     */
+    is_err(): this is Err<T, E>;
+    /**
+     * Maps a `Result<T, E>` into a `Result<U, E>`.
+     * Will return `Ok(fn(value))` if the result is `Ok`, or propagate `Err`.
+     * @param fn function to transform the inner value
+     */
+    map(): Err<T, E>;
+    /**
+     * Maps a `Result<T, E>` into a `Result<T, F>`.
+     * Will propagate `Ok`, or return `Err(fn(error))` if the result is `Err`.
+     * @param fn function to transform the inner value
+     */
+    map_err<F>(fn: (arg: E) => F): Err<T, F>;
+    /**
+     * Pattern-match on the result, running the `Ok` or `Err` function in the respective cases.
+     * @param matcher object containing an `Ok(value)` and `Err(error)` function to run
+     */
+    match<R>(matcher: { Ok(value: T): R, Err(error: E): R }): R;
+    /**
+     * Pattern-match on the result, running the former or latter function
+     * in the `Ok` and `Err` cases respectively.
+     * 
+     * @param on_ok function to run if the result is `Ok(value)`
+     * @param on_err function to run if the option is `Err(error)`
+     */
+    match<R>(on_ok: (value: T) => R, on_err: (error: E) => R): R;
+    /**
+     * Return the contained `Ok` value. Will throw a hard error if called on `Err`. 
+     */
+    unwrap(): never;
+    /**
+     * Return the contained `Ok` value, or `default_value` if called on `Err`. 
+     * @param default_value value to use if the result is `Err`
+     */
+    unwrap_or<T>(default_value: T): T;
+    /**
+     * Return the contained `Ok` value, or the result of `otherwise` if called on `Err`.
+     * @param otherwise function to use if the result is `Err`
+     */
     unwrap_or_else<T>(otherwise: (err: E) => T): T;
 }
 
 export const Ok = <T, E>(value: T) => ({
-    // TODO: expect
+    expect(_) { return value; },
     is_ok() { return true; },
     is_err() { return false; },
     map(fn) { return Ok(fn(value)); },
     map_err() { return this; },
     unwrap() { return value; },
-    unwrap_or() { return value; },
-    unwrap_or_else() { return value; },
+    unwrap_or(_) { return value; },
+    unwrap_or_else(_) { return value; },
     match<R>(matcher_or_ok) {
-        if (("Ok" in matcher_or_ok) && ("Err" in matcher_or_ok)) {
+        if ("Ok" in matcher_or_ok) {
             return (matcher_or_ok as { Ok: (value: T) => R }).Ok(value);
         }
         return matcher_or_ok(value);
@@ -49,21 +156,19 @@ export const Ok = <T, E>(value: T) => ({
     toString() { return `Ok(${value})`; },
 }) as Ok<T, E>;
 export const Err = <T, E>(error: E) => ({
-    // TODO: expect
+    expect(message) { throw Error(message) },
     is_ok() { return false; },
     is_err() { return true; },
     map() { return this; },
     map_err(fn) { return Err(fn(error)); },
-    unwrap(_) { throw Error("Cannot call unwrap on an Err value") },
+    unwrap() { throw Error("Cannot call unwrap on an Err value") },
     unwrap_or(default_value) { return default_value; },
     unwrap_or_else(otherwise) { return otherwise(error); },
     match<R>(matcher_or_ok, err) {
-        if (("Ok" in matcher_or_ok) && ("Err" in matcher_or_ok)) {
+        if ("Err" in matcher_or_ok) {
             return (matcher_or_ok as { Err: (error: E) => R }).Err(error);
         }
         return err(error);
     },
     toString() { return `Err(${error})`; },
 }) as Err<T, E>;
-
-declare let a: Result<string, Error>;
